c95d46432379ccadda83565492462ed27df5deb1
totoropcbeta <957584602@qq.com>
2020-06-16 23:09:31 +0800

    添加联系人Page

--- Page/contacts_page.py
+++ Page/contacts_page.py
@@ -4,6 +4,15 @@ from Config.contact_info_xpath import *
 
 class AddContactPage(BasePage):
     """添加联系人Page"""
+    def cancel_failure(self):
+        text_source = self.by_class("android.widget.TextView")
+        text_value = []
+        for i in text_source:
+            text_value.append(i.text)
+        text_ass = "".join(text_value)
+        if "未发送成功的邮件" in text_ass:
+            self.by_id("alert_dialog_content_msg").click()
+
     def init_add(self):
         # 底部通讯录按钮
         contact_button = self.by_uiautomator('new UiSelector().text("通讯录")')
@@ -28,12 +37,17 @@ class AddContactPage(BasePage):
         if position:
             el_pos = self.by_xpath(xpath5)
             el_pos.send_keys(position)
+
         if phone:
             el_pho = self.by_xpath(xpath6)
             el_pho.send_keys(phone)
         if addr:
             el_addr = self.by_xpath(xpath7)
             el_addr.send_keys(addr)
+        if email and phone and addr:
+            el1 = self.by_xpath(pos_xpath)
+            el2 = self.by_xpath(com_xpath)
+            self.drag_drop(el1, el2)
         if note:
             el_note = self.by_xpath(xpath8)
             el_note.send_keys(note)
